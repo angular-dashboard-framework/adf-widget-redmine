f8cc1f94fc8135df0e5034928f31193f0a19d087
Christoph Wolfes <christoph.wolfes@triology.de>
2017-05-16 15:01:38 +0200

    added a test structure for all angular components

---
+++ src/test/chart/chartControllerSpec.js
@@ -0,0 +1,12 @@
+describe('Redmine Chart-Controller', function () {
+
+  beforeEach(module('adf.widget.redmine'));
+
+  it('test jasmine configuration',function(){
+    var number = 9;
+    expect(number).toBeCloseTo(9);
+  });
+
+});
+
+
